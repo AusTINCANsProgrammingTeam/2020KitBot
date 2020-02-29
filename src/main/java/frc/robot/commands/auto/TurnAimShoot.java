b0b0440ffcddebaed2c108e073ff655a902f2ec8
unknown <zsim314@gmail.com>
2020-02-29 16:05:39 -0600

    eaj

---
+++ src/main/java/frc/robot/commands/auto/TurnAimShoot.java
@@ -0,0 +1,86 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.auto;
+
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.commands.drive.DriveCommand;
+import frc.robot.*;
+
+import java.util.logging.Logger;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class TurnAimShoot extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private static final Logger LOGGER = Logger.getLogger(DriveCommand.class.getName());
+  double tx = SmartDashboard.getNumber("LimelightX", 0);
+  double ty = SmartDashboard.getNumber("LimelightY", 0);
+  double d = 0;
+  
+  double minSteerAdjust = .2;
+  double steeringAdjust = 0.0;
+  double headingCommand = 0;
+  double p = .013;
+  boolean seen = false;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public TurnAimShoot() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.mDriveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  // tx is the degrees the limelight detects we are off by the target 
+  // adjust p until it works
+  @Override
+  public void execute() {
+    RobotContainer.light.setValue(Constants.LL_LIGHT_ON);
+    tx = SmartDashboard.getNumber("LimelightX", 0);
+    ty = SmartDashboard.getNumber("LimelightY", 0);
+    if(tx>1.0){
+        steeringAdjust = p * tx +minSteerAdjust;
+        seen = true;
+    }
+    else if(tx<-1.0){
+        steeringAdjust = p * tx -minSteerAdjust;
+        seen = true;
+    }
+     
+    if(tx != 0)
+      RobotContainer.mDriveSubsystem.arcadeDrive(0,steeringAdjust);
+
+      steeringAdjust = 0;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.light.setValue(Constants.LL_LIGHT_OFF);
+    seen = false;
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (tx < 1 && tx > -1 && seen == true);
+  }
+}
---
+++ src/main/java/frc/robot/commands/shooter/ShootCommand.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.shooter;
+
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class ShootCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ShootCommand() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.mShooterSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      double speed = SmartDashboard.getNumber("shooter speed", 0);
+      RobotContainer.mShooterSubsystem.setVelocitySetpoint(speed*Constants.maxRPMShooter);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+      RobotContainer.mShooterSubsystem.setSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
